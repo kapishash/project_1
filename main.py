19d32ba8eff71b31b12057b5f1f30c7108d5bb61
kapishash <kapishashtankar10@gmail.com>
2025-01-13 17:25:03 +0530

    till transformation

--- main.py
+++ main.py
@@ -1,3 +1,42 @@
 from src.project_1 import logger 
+from src.project_1.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.project_1.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+from src.project_1.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 
-logger.info("Welcome to our custom logging for project 1")
+
+STAGE_NAME="Data Ingestion Stage"
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<") 
+        data_ingestion = DataIngestionTrainingPipeline()
+        data_ingestion.initiate_data_ingestion()
+        logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx==========x")
+    except Exception as e:  
+        logger.exception(e)
+        raise e
+    
+STAGE_NAME="Data Validation Stage"
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+        data_validation = DataValidationTrainingPipeline()
+        data_validation.initiate_data_validation()
+        logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+STAGE_NAME="Data Transformation Stage"
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+        obj = DataTransformationTrainingPipeline()
+        obj.initiate_data_transformation()
+        logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
---
+++ src/project_1/components/data_ingestion.py
@@ -0,0 +1,27 @@
+import os
+import urllib.request as request
+from src.project_1 import logger
+import zipfile
+from src.project_1.entity.config_entity import DataIngestionConfig
+
+
+
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
+    def download_file(self):
+        if not os.path.exists(self.config.local_data_file):
+            filename, header=request.urlretrieve(
+                url=self.config.source_URL,
+                filename=self.config.local_data_file
+                )
+            logger.info(f"{filename} download! with following info: \n{header}")
+        else:
+            logger.info(f"File already exists of size: {os.path.getsize(self.config.local_data_file)}")
+
+    def extract_file(self):
+        unzip_path=self.config.unzip_dir
+        os.makedirs(unzip_path, exist_ok=True)
+        with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
+            zip_ref.extractall(unzip_path)
