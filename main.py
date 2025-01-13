c0daf6f37f4d554dac6fc98845431bf6277940ba
kapishash <kapishashtankar10@gmail.com>
2025-01-13 21:34:00 +0530

    model training

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.project_1 import logger
 from src.project_1.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.project_1.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.project_1.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.project_1.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 
 STAGE_NAME="Data Ingestion Stage"
@@ -40,3 +41,15 @@ if __name__ == "__main__":
         logger.exception(e)
         raise e
 
+    
+STAGE_NAME="Model Training Stage"
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+        obj = ModelTrainerTrainingPipeline()
+        obj.initiate_model_training()
+        logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
